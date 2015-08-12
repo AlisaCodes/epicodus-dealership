a598ebc33946c0163dd256d6d9f6cdaa5e313a6c
Alisa Colon & Vaughn Dice <student@epicodus.com>
2015-08-12 14:45:44 -0700

    Add passed spec for dealership's bicycle list and form paths

--- spec/dealership_integration_spec.rb
+++ spec/dealership_integration_spec.rb
@@ -43,3 +43,24 @@ describe("the dealership bicycle list path", {:type => :feature}) do
     expect(page).to have_content("Here are the bikes at the Recycled Cycles dealership:")
   end
 end
+
+describe("the dealership bicycle form path", {:type => :feature}) do
+  it("takes the user to a form to input a bicycle") do
+    visit('/dealerships/1')
+    click_link('Add New Bicycle')
+    expect(page).to have_content("Add Bicycles to Recycled Cycles!")
+  end
+end
+
+describe("the add bicycle form to dealership's bicycle list path", {:type => :feature}) do
+  it("submits the form and takes user to dealership's bicycle list") do
+    visit('/bicycles/1/new')
+    fill_in('make', :with => 'Trek')
+    fill_in('model', :with => 'Monster')
+    fill_in('year', :with => '2013')
+    click_button('Add Bike')
+    expect(page).to have_content("Trek")
+    expect(page).to have_content("Monster")
+    expect(page).to have_content("2013")
+  end
+end
