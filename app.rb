b36ff82ae63b8ed85d987d4e4c33b934d4ce58d8
Alisa Colon & Vaughn Dice <student@epicodus.com>
2015-08-12 11:47:36 -0700

    add initial views and app.rb

---
+++ app.rb
@@ -0,0 +1,24 @@
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/bicycle.rb')
+require('./lib/dealership.rb')
+
+get("/") do
+  erb(:index)
+end
+
+get('/dealerships') do
+  @dealerships = Dealership.all()
+  erb(:dealerships)
+end
+
+get('/dealerships/new') do
+  erb(:dealer_form)
+end
+
+post '/dealerships' do
+  new_dealer = Dealership.new(params.fetch("name"))
+  new_dealer.save()
+  @dealerships = Dealership.all()
+  erb(:dealerships)
+end
