29b937e1985b4df47d3de651b49a1267acae74cc
Alisa Colon & Vaughn Dice <student@epicodus.com>
2015-08-12 09:41:40 -0700

    Add spec and method for bikes and id

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -1,9 +1,19 @@
 class Dealership
   define_method(:initialize) do |name|
     @name = name
+    @id = 1
+    @bikes = []
   end
 
   define_method(:name) do
     @name
+  end
+
+  define_method(:id) do
+  @id
+  end
+
+  define_method(:bikes) do
+  @bikes
   end
 end
--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -8,4 +8,19 @@ describe(Dealership) do
       expect(test_dealership.name()).to(eq("Recycled Cycles"))
     end
   end
+
+  describe('#id') do
+    it('returns the id of the dealership') do
+      test_dealership = Dealership.new("Recycled Cycles")
+      expect(test_dealership.id()).to(eq(1))
+    end
+  end
+
+  describe('#bikes') do
+    it('initially returns an empty array of bicycles for the dealership') do
+      test_dealership = Dealership.new("Recycled Cycles")
+      expect(test_dealership.bikes()).to(eq([]))
+    end
+  end
+
 end
